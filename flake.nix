04c52d41cd6cb551a8132a4d626ad7f65ebb7f6c
Jan T. Sott <git@idleberg.com>
2025-10-08 09:10:19 +0200

    chore: add clear

--- flake.nix
+++ flake.nix
@@ -56,40 +56,41 @@
           which
         ];
         shellHook = ''
-          macchina
+          clear
+               macchina
 
-          echo "Setting up environment variables..."
-          export EDITOR=$(which nvim)
-          export VISUAL=$(which code)
+               echo "Setting up environment variables..."
+               export EDITOR=$(which nvim)
+               export VISUAL=$(which code)
 
-          echo "Setting up shell aliases..."
-          alias ..="cd .."
-          alias cat=bat
-          alias ls="eza -lh --group-directories-first --icons=auto"
-          alias ll="ls -la"
-          alias vim=nvim
+               echo "Setting up shell aliases..."
+               alias ..="cd .."
+               alias cat=bat
+               alias ls="eza -lh --group-directories-first --icons=auto"
+               alias ll="ls -la"
+               alias vim=nvim
 
-          if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-            echo "Setting up Git..."
-            git config alias.co checkout
-            git config alias.br branch
-            git config alias.ci commit
-            git config alias.st status
-            git config alias.amend "commit --amend -m"
-            git config alias.unstage "restore --staged"
-            git config help.autocorrect 50
-          else
-            gum log -sl warn "Not inside a Git repository, skipping config setup"
-          fi
+               if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+                 echo "Setting up Git..."
+                 git config alias.co checkout
+                 git config alias.br branch
+                 git config alias.ci commit
+                 git config alias.st status
+                 git config alias.amend "commit --amend -m"
+                 git config alias.unstage "restore --staged"
+                 git config help.autocorrect 50
+               else
+                 gum log -sl warn "Not inside a Git repository, skipping config setup"
+               fi
 
-          if [ -f "package.json" ]; then
-            corepack install
-            pnpm install --frozen-lockfile
+               if [ -f "package.json" ]; then
+                 corepack install
+                 pnpm install --frozen-lockfile
 
-            echo -e "\nCurrent Project: $(jq -r '.name' package.json)"
-          else
-            gum log -sl warn "No package.json found, skipping installation step"
-          fi
+                 echo -e "\nCurrent Project: $(jq -r '.name' package.json)"
+               else
+                 gum log -sl warn "No package.json found, skipping installation step"
+               fi
         '';
       };
     });
